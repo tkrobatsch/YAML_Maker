bc598443ed7db2aade1480f3b958c2e24004bed5
cnattras <saccharomyces.cerevisae@gmail.com>
2019-03-05 13:04:37 -0500

    fixing the test from class

--- MakeYAML.cpp
+++ MakeYAML.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include "MakeYAML.h"
 
-HI HI HI
 void MakeYAML(std::string file_name, std::string x_header, std::string y_header, std::vector<std::string>qualifiers, std::vector<double>x, std::vector<double>y, std::vector<double>ex, std::vector<double>ey, std::string xerr_label, std::string yerr_label) {
 	std::ofstream fout;
 	fout.open(file_name.c_str());
